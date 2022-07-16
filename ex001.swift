50401fdf7382246aa9547b8b72c0b3859da5e1a3
ksergeyru <i@ksergey.ru>
2022-07-16 18:32:58 +0300

    ex001.swift

---
+++ ex001.swift
@@ -0,0 +1,10 @@
+/*
+Дано число. Проверить кратно ли оно 7 и 23 
+*/
+// var n = Int(readLine()!)!
+// var flag = n % 7 == 0 && n % 23 == 0
+// print(flag)
+
+
+var n = Int(readLine()!)!
+print(n % 7 == 0 && n % 23 == 0)
