b7af5dd0bb2fb61e13d83b112d3f33170f794b33
ksergeyru <i@ksergey.ru>
2022-07-16 18:54:37 +0300

    add ex003.cs

---
+++ ex003.cs
@@ -0,0 +1,13 @@
+﻿/*
+
+Найти расстояние между точками в пространстве 2D
+
+*/
+//        
+double[] x = { 0, 1 };
+double[] y = { 1, 0 };
+
+double ab = x[0] - y[0];
+double ac = x[1] - y[1];
+var d = Math.Sqrt(ab * ab + ac * ac);
+Console.WriteLine($"d = {d}");
